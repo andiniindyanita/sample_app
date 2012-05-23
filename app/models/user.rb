80fa88352c86194b6b36bfeb906c6a3e8f64c2d6
Andini Indyanita <andini.indyanita@gmail.com>
2012-05-23 14:34:08 +0700

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
+  before_save { self.email.downcase! }
   before_save { |user| user.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
